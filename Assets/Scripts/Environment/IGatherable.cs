0c5673fa24fcab123ff3fc3575c14f1381fb5204
Chris Kurhan <chris.kurhan@gamedev.tv>
2024-10-15 16:57:29 -0500

    03_BT_URTS Gatherable Supplies

---
+++ Assets/Scripts/Environment/IGatherable.cs
@@ -0,0 +1,12 @@
+namespace GameDevTV.RTS.Environment
+{
+    public interface IGatherable
+    {
+        public SupplySO Supply { get; }
+        public int Amount { get; }
+        public bool IsBusy { get; }
+
+        public bool BeginGather();
+        public int EndGather();
+    }
+}
