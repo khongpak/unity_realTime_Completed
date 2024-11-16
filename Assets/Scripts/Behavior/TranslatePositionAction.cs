c00314085219e750dd6548bf9420c33d41012015
Chris Kurhan <chris.kurhan@gamedev.tv>
2024-11-16 07:31:32 -0600

    06_BB_URTS Animate Worker While Building

--- Assets/Scripts/Behavior/TranslatePositionAction.cs
+++ Assets/Scripts/Behavior/TranslatePositionAction.cs
@@ -3,6 +3,8 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using UnityEngine.AI;
+using GameDevTV.RTS.Utilities;
 
 namespace GameDevTV.RTS.Behavior
 {
@@ -14,19 +16,55 @@ namespace GameDevTV.RTS.Behavior
         [SerializeReference] public BlackboardVariable<Vector3> TargetLocation;
         [SerializeReference] public BlackboardVariable<float> Speed;
 
+        private Animator animator;
+        private NavMeshAgent agent;
+        private float endTime;
+        private Vector3 direction;
+        private Transform selfTransform;
+
         protected override Status OnStart()
         {
+            if (Self.Value == null) return Status.Failure;
+
+            animator = Self.Value.GetComponent<Animator>();
+            if (Self.Value.TryGetComponent(out agent))
+            {
+                agent.enabled = false;
+            }
+
+            selfTransform = Self.Value.transform;
+            float distance = Vector3.Distance(selfTransform.position, TargetLocation.Value);
+            endTime = Time.time + distance / Speed;
+            direction = (TargetLocation.Value - selfTransform.position).normalized;
+
+            selfTransform.forward = direction;
+
             return Status.Running;
         }
 
         protected override Status OnUpdate()
         {
-            return Status.Success;
+            if (Time.time > endTime) return Status.Success;
+
+            if (animator != null)
+            {
+                animator.SetFloat(AnimationConstants.SPEED, Speed);
+            }
+
+            selfTransform.position += Speed * Time.deltaTime * direction;
+            return Status.Running;
         }
 
         protected override void OnEnd()
         {
+            if (animator != null)
+            {
+                animator.SetFloat(AnimationConstants.SPEED, 0);
+            }
+            if (agent != null)
+            {
+                agent.enabled = true;
+            }
         }
     }
-
 }
