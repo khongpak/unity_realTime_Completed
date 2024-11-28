f02afb011d84b9e4aae96b498a949d0c95f8d017
Chris Kurhan <chris.kurhan@gamedev.tv>
2024-11-28 13:25:43 -0600

    15_BB_URTS Distance Checking Restrictions

--- Assets/Scripts/Commands/BuildingRestrictionSO.cs
+++ Assets/Scripts/Commands/BuildingRestrictionSO.cs
@@ -6,15 +6,27 @@ namespace GameDevTV.RTS.Commands
     [CreateAssetMenu(fileName = "Building Restriction", menuName = "Buildings/Restrictions", order = 7)]
     public class BuildingRestrictionSO : ScriptableObject
     {
+        [field: SerializeField] public float Radius { get; private set; } = 1f;
+        [field: SerializeField] public LayerMask LayerMask { get; private set; }
+        [field: SerializeField] public OverlapStyle HitDetectionStyle { get; private set; } = OverlapStyle.Sphere;
+
         [field: SerializeField] public bool MustBeFullyOnNavmesh { get; private set; } = true;
         [field: SerializeField] public int NavMeshAgentTypeId { get; private set; }
         [field: SerializeField] public float NavMeshTolerance { get; private set; } = 0.1f;
 
         [field: SerializeField] public Vector3 Extents { get; private set; } = Vector3.one;
 
+        private Collider[] hitColliders = new Collider[1];
+
         public bool CanPlace(Vector3 position)
         {
-            bool isOnNavMesh = true;
+            int hits = HitDetectionStyle switch
+            {
+                OverlapStyle.Sphere => Physics.OverlapSphereNonAlloc(position, Radius, hitColliders, LayerMask),
+                OverlapStyle.Box => Physics.OverlapBoxNonAlloc(
+                    position, Extents, hitColliders, Quaternion.identity, LayerMask
+                )
+            };
 
             if (MustBeFullyOnNavmesh)
             {
@@ -24,12 +36,12 @@ namespace GameDevTV.RTS.Commands
                     agentTypeID = NavMeshAgentTypeId
                 };
 
-                isOnNavMesh = IsFullyOnNavMesh(position, queryFilter);
+                bool isOnNavMesh = IsFullyOnNavMesh(position, queryFilter);
 
-                return isOnNavMesh;
+                return hits == 0 && isOnNavMesh;
             }
 
-            return true;
+            return hits == 0;
         }
 
         private bool IsFullyOnNavMesh(Vector3 position, NavMeshQueryFilter queryFilter)
@@ -47,6 +59,12 @@ namespace GameDevTV.RTS.Commands
                                 position + new Vector3(-Extents.x, 0, Extents.z),
                                 out NavMeshHit _, NavMeshTolerance, queryFilter);
             return isOnNavMesh;
+        }
+
+        public enum OverlapStyle
+        {
+            Sphere,
+            Box
         }
     }
 }
