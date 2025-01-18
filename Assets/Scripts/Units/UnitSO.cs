2c5f6f0de30fa5201a9c76daeb5c26a7cf643746
Chris Kurhan <chris.kurhan@gamedev.tv>
2025-01-18 08:47:48 -0600

    15_MU_URTS Unit Transport Configuration

--- Assets/Scripts/Units/UnitSO.cs
+++ Assets/Scripts/Units/UnitSO.cs
@@ -6,5 +6,6 @@ namespace GameDevTV.RTS.Units
     public class UnitSO : AbstractUnitSO
     {
         [field: SerializeField] public AttackConfigSO AttackConfig { get; private set; }
+        [field: SerializeField] public TransportConfigSO TransportConfig { get; private set; }
     }
 }
